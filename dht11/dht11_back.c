a134ba2f44846bd13c763d699845e431bad7c019
MaciejZakrzewski <zakrzewski496@gmail.com>
2017-01-12 20:24:30 +0100

    Poprawiono formatowanie.

--- dht11/dht11_back.c
+++ dht11/dht11_back.c
@@ -17,15 +17,15 @@
 // ---------------------------------------------------
 // DHT11 FUNCTIONS
 // ---------------------------------------------------
-int dht11_val[5]={0,0,0,0,0};
+int dht11_val[5] = {0,0,0,0,0};
 
 int dht11_read_val(int *h, int *t) {
-  uint8_t lststate=HIGH;
-  uint8_t counter=0;
-  uint8_t j=0,i;
+  uint8_t lststate = HIGH;
+  uint8_t counter = 0;
+  uint8_t j = 0, i;
   
-  for (i=0;i<5;i++) {
-     dht11_val[i]=0;
+  for (i = 0; i < 5; i++) {
+     dht11_val[i] = 0;
   }
 
   pinMode(DHT11PIN,OUTPUT);
@@ -35,8 +35,8 @@ int dht11_read_val(int *h, int *t) {
   delayMicroseconds(40);
   pinMode(DHT11PIN,INPUT);
 
-  for (i=0; i < MAX_TIME; i++) {
-    counter=0;
+  for (i = 0; i < MAX_TIME; i++) {
+    counter = 0;
     while (digitalRead(DHT11PIN) == lststate){
       counter++;
       delayMicroseconds(1);
@@ -44,22 +44,22 @@ int dht11_read_val(int *h, int *t) {
         break;
       }
     }
-    lststate=digitalRead(DHT11PIN);
+    lststate = digitalRead(DHT11PIN);
     if (counter == 255) {
        break;
     }
     // top 3 transitions are ignored
     if ((i >= 4) && (i%2 == 0)) {
-      dht11_val[j/8]<<=1;
-      if(counter>16) {
-        dht11_val[j/8]|=1;
+      dht11_val[j/8] <<= 1;
+      if(counter > 16) {
+        dht11_val[j/8] |= 1;
       }
       j++;
     }
   }
 
   // verify cheksum and print the verified data
-  if ((j>=40) && (dht11_val[4]==((dht11_val[0]+dht11_val[1]+dht11_val[2]+dht11_val[3])& 0xFF))) {
+  if ((j >= 40) && (dht11_val[4] == ((dht11_val[0] + dht11_val[1] + dht11_val[2] + dht11_val[3]) & 0xFF))) {
     // Only return the integer part of humidity and temperature. The sensor
     // is not accurate enough for decimals anyway 
     *h = dht11_val[0];
